b4b7c9bf3167c6373df9d45d48c03ec686d93236
umakantmane22 <umakantmane22@gmail.com>
2026-04-03 17:03:21 +0530

    Added new code also modify existing code.

--- src/main/java/com/mane/umakant/java/interviewQuestions/FindCommonCharactersFromGivenTwoString.java
+++ src/main/java/com/mane/umakant/java/interviewQuestions/FindCommonCharactersFromGivenTwoString.java
@@ -9,15 +9,15 @@ public class FindCommonCharactersFromGivenTwoString {
         // Q33: Write a program to find common characters from given two string
         // input 123456, 162, 21 op: common chars from first 2 string:1,2,6 & from all 3 is: 1,2
         // https://www.youtube.com/watch?v=axXg624LaC8
-        Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in); // 👉 Used to take input from user
         System.out.println("Please enter first string");
         String firstStringInput = sc.next();
         System.out.println("Please enter second string");
         String secondStringInput = sc.next();
         System.out.println("Please enter third string");
-        String thirdStringInput = sc.next();
+        String thirdStringInput = sc.next(); //👉 Takes 3 strings (no spaces allowed because of next())
         System.out.println("firstStringInput: " + firstStringInput + " secondStringInput: " + secondStringInput + " thirdStringInput: " + thirdStringInput);
-        List<Character> commonChars = findCommonCharacters(firstStringInput, secondStringInput);
+        List<Character> commonChars = findCommonCharacters(firstStringInput, secondStringInput); //👉 Stores final result
         System.out.println("Common characters from first and second input string:: " + commonChars);
         List<Character> commonChars3InputStrings = findCommonCharacters(firstStringInput, secondStringInput, thirdStringInput);
         System.out.println("Common characters from first and second third input string:: " + commonChars3InputStrings);
@@ -25,18 +25,36 @@ public class FindCommonCharactersFromGivenTwoString {
 
     private static List<Character> findCommonCharacters(String firstStringInput, String secondStringInput) {
         // Convert strings to char arrays
-        char[] charArray1 = firstStringInput.toCharArray();
-        char[] charArray2 = secondStringInput.toCharArray();
+        char[] charArray1 = firstStringInput.toCharArray(); // . Convert first string to char array
+        char[] charArray2 = secondStringInput.toCharArray();    // . Convert second string to char array
 
         List<Character> commonChars = new ArrayList<>();
         // Check each character from the first string
-        for (char c : charArray1) {
+        for (char c : charArray1) { // 👉 Iterate over each character of first string only
             // If character exists in the second string and is not already added to the common list
             if (secondStringInput.indexOf(c) != -1 && !commonChars.contains(c)) {
                 commonChars.add(c);
             }
         }
         return commonChars;
+        /*
+        Note:
+        Condition:
+        secondStringInput.indexOf(c) != -1
+        👉 Checks:
+        Is character c present in second string?
+
+        ✔ If found → returns index (0,1,2...)
+        ❌ If not found → returns -1
+
+        🧩 Condition 3 (Very Important)
+        !commonChars.contains(c)
+
+        👉 Ensures:
+
+        No duplicate characters in result
+
+         */
     }
 
     private static List<Character> findCommonCharacters(String firstStringInput, String secondStringInput, String thirdStringInput) {
--- src/main/java/com/mane/umakant/java/interviewQuestions/ReverseString.java
+++ src/main/java/com/mane/umakant/java/interviewQuestions/ReverseString.java
@@ -4,55 +4,67 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class ReverseString {
-    public static void main(String a[]){
+    public static void main(String a[]) {
         //Q34: Write a program to find reverse of given string
         // input String start = "123456";
         // OP
         //reverseStringJava7: 654321
 
-        String inputString="123456";
-        System.out.println("input string:: "+inputString);
+        String inputString = "123456";
+        System.out.println("input string:: " + inputString);
 
-        StringBuffer reversedUsingStringBuffer=new StringBuffer();
+        StringBuffer reversedUsingStringBuffer = new StringBuffer();
         String reversedUsingString = "";
         System.out.println("Reverse chars in string:: ");
-        for (int i=0;i<inputString.length();i++) {
-            System.out.print(inputString.charAt(inputString.length()-i-1)+" ");
+        for (int i = 0; i < inputString.length(); i++) {
+            System.out.print(inputString.charAt(inputString.length() - i - 1) + " ");
 
-            reversedUsingStringBuffer.append(inputString.charAt(inputString.length()-i-1)+" ");
-            reversedUsingString=reversedUsingString+inputString.charAt(inputString.length()-i-1)+" ";
+            reversedUsingStringBuffer.append(inputString.charAt(inputString.length() - i - 1) + " ");
+            reversedUsingString = reversedUsingString + inputString.charAt(inputString.length() - i - 1) + " ";
         }
         System.out.println("");
-        System.out.println("reversedUsingStringBuffer:: "+reversedUsingStringBuffer);
-        System.out.println("reversedUsingString:: "+reversedUsingString);
+        System.out.println("reversedUsingStringBuffer:: " + reversedUsingStringBuffer);
+        System.out.println("reversedUsingString:: " + reversedUsingString);
         // or
-        char[]charArray=inputString.toCharArray();
+        char[] charArray = inputString.toCharArray();
         // Initialize an empty string to store the reversed string
         String reversed = "";
         // Traverse the array from the end to the beginning
         for (int i = charArray.length - 1; i >= 0; i--) {
             reversed += charArray[i];
         }
-        System.out.println("reversedUsingChatGpt:: "+reversed);
+        System.out.println("reversedUsingChatGpt:: " + reversed);
 
         // or using Java8
         List<String> reversedUsingJava8 = Arrays.stream(inputString.split(""))
                 .sorted(Collections.reverseOrder())
                 .collect(Collectors.toList());
 
-        System.out.println("reversedUsingJava8:: "+reversedUsingJava8);
+        System.out.println("reversedUsingJava8:: " + reversedUsingJava8);
 
-        String reversedUsingJava8InStringFormat1=Arrays.stream(inputString.split(""))
+        String reversedUsingJava8InStringFormat1 = Arrays.stream(inputString.split(""))
                 .sorted(Collections.reverseOrder())
                 .collect(Collectors.joining());
-        System.out.println("reversedUsingJava8InStringFormat1:: "+reversedUsingJava8InStringFormat1);
+        System.out.println("reversedUsingJava8InStringFormat1:: " + reversedUsingJava8InStringFormat1);
 
         String reversedUsingJava8InStringFormat2 = inputString.chars().mapToObj(c -> (char) c)
                 .sorted(Collections.reverseOrder())
                 .map(String::valueOf)
                 .collect(Collectors.joining());
-        System.out.println("reversedUsingJava8InStringFormat2:: "+reversedUsingJava8InStringFormat2);
+        System.out.println("reversedUsingJava8InStringFormat2:: " + reversedUsingJava8InStringFormat2);
+        String reversed2 = IntStream.rangeClosed(1, inputString.length())
+                .mapToObj(i -> inputString.charAt(inputString.length() - i))
+                .map(String::valueOf)
+                .reduce("", String::concat);
+
+        System.out.println("Reversed: " + reversed2);
+
+        String revString = Arrays.stream(inputString.split(""))
+                .sorted(Collections.reverseOrder())
+                .collect(Collectors.joining());
+        System.out.println("revString:: " + revString);
     }
 }
